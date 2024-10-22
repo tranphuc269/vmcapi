815895b16d2ff1ca906b0754e6885e7e52be7be1
tranphuc <phuc260900@gmail.com>
2024-10-22 19:41:18 +0700

    fix cors

---
+++ src/main/java/com/vai/vmcapi/security/DCorsConfiguration.java
@@ -0,0 +1,29 @@
+package com.vai.vmcapi.security;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class DCorsConfiguration {
+
+    @Bean
+    public FilterRegistrationBean dgCorsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        org.springframework.web.cors.CorsConfiguration config = new org.springframework.web.cors.CorsConfiguration();
+        config.addAllowedOriginPattern("*");
+        config.setAllowCredentials(true);
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+
+        //bean.setOrder(-110);
+        return new FilterRegistrationBean(new CorsFilter(source));
+
+    }
+}
